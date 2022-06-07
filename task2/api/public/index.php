2ce18b2eeaad5f662a3cf17716f63ec59a1a3d7c
neu_net <przman2000@mail.ru>
2022-06-07 14:58:20 +1000

    unused commented code deleted

--- task2/api/public/index.php
+++ task2/api/public/index.php
@@ -19,11 +19,3 @@ $request = array(
 $application = new Application();
 
 $application->run($request);
-
-
-//$link = mysqli_connect('mysql', 'root', 'root');
-//if (!$link) {
-//	die('Ошибка соединения: ' . mysqli_error());
-//}
-//echo 'Успешно соединились';
-//mysqli_close($link);
